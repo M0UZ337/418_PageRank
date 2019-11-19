3ee93944f73c7ed5a04ccb6593fa22dc0e74cfde
Nulllun <michaeld23046e123@gmail.com>
2019-11-19 15:59:33 +0800

    modified fields in class PDNodeWritable

--- PDNodeWritable.java
+++ PDNodeWritable.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import org.apache.hadoop.io.Writable;
 import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.BooleanWritable;
 import org.apache.hadoop.io.MapWritable;
@@ -11,25 +12,37 @@ import org.apache.hadoop.io.MapWritable;
 
 public class PDNodeWritable implements Writable {
     
-    public IntWritable nodeID;
-    public IntWritable mass;
-    public MapWritable adjList;
+    private IntWritable nodeID;
+    private DoubleWritable prValue;
+    private MapWritable adjList;
     
-    public PDNodeWritable(IntWritable nodeID, IntWritable mass, MapWritable adjList) {
+    public PDNodeWritable(IntWritable nodeID, DoubleWritable prValue, MapWritable adjList) {
         this.nodeID = nodeID;
-        this.mass = mass;
+        this.prValue = prValue;
         this.adjList = adjList;
+    }
+
+    public DoubleWritable getPRValue(){
+        return prValue;
+    }
+
+    public void setPRValue(DoubleWritable prValue){
+        this.prValue = prValue;
+    }
+
+    public MapWritable getAdjList(){
+        return adjList;
     }
 
     public void write(DataOutput out) throws IOException {
         nodeID.write(out);
-        mass.write(out);
+        prValue.write(out);
         adjList.write(out);
     }
 
     public void readFields(DataInput in) throws IOException {
         nodeID.readFields(in);
-        mass.readFields(in);
+        prValue.readFields(in);
         adjList.readFields(in);
     }
 }
